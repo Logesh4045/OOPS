2e651e6547ad53bbbf0692d5f04577723a65c40d
Logesh4045 <logesh.noyyal@gmail.com>
2023-01-10 15:58:19 +0530

    Updated

--- UseDoctor.java
+++ UseDoctor.java
@@ -4,6 +4,8 @@ public class UseDoctor {
 	public static void main(String[]args) {
 		Doctor d=new Doctor("LOgesh",25,"MALE","bE",20000,"20BGE" ) ;
 		System.out.println(d);
+		System.out.println(/*******************/);
+		
 	}
 	
 	}
